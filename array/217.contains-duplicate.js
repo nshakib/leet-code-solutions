139b4cec48d6d4366c7bca6880f4f22d8aa73e59
nshakib <nazmus.shakib.cse@gmail.com>
2026-01-27 14:32:27 +0600

    leet code 217.contains-duplicate solution

---
+++ array/217.contains-duplicate.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function(nums) {
+    let set = new Set();
+
+    for (let num of nums){
+        if(set.has(num)){
+            return true
+        }
+        set.add(num)
+    }
+    return false;    
+}
